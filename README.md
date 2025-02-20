61660bc8f0d1def5056b1ccd961c18e2211d47f5
Danny van der Knaap <dannyvdknaap@gmail.com>
2025-02-20 12:30:32 +0100

    chore(install): add install script

--- README.md
+++ README.md
@@ -4,20 +4,62 @@ Use Rofi and xdotool to automate typing and run background tasks in Kali. It wor
 This is skeleton code with a few examples. Everyone is welcome to contribute.
 
 ## Installation
-- Clone repo to ~/Desktop/base in Kali
-  
-`git clone git@github.com:spipm/rofi-hacking-helper.git ~/Desktop/base`
-- Go to `Settings Manager`->`Keyboard`->`Application Shortcuts` and add a shortcut, like Ctrl+Shift+M to:
+### Automated Installation
 
-`/home/kali/Desktop/base/code/xdotool/rofisearch_scripts_menu.sh`
-- Install `sudo apt install rofi -y` if not already installed
-- Press Ctrl+Shift+M and execute scripts from anywhere
+1. Clone this repository and navigate to it:
+```bash
+git clone https://github.com/dvdknaap/rofi-hacking-helper.git ~/Desktop/base
+cd ~/Desktop/base
+```
+
+2. Run the `install.sh` script:
+```bash
+bash install.sh
+```
+
+### Manual Installation
+
+1. Clone this repository and navigate to it:
+```bash
+git clone https://github.com/dvdknaap/rofi-hacking-helper.git ~/Desktop/base
+cd ~/Desktop/base
+```
+
+2. Install Rofi if not already installed
+``` bash
+sudo apt install rofi -y
+```
+
+3. Add rofisearch script menu as keyboard shortcut:
+- Go to `Settings Manager`->`Keyboard`->`Application Shortcuts` / `Keyboard shortcuts`:
+![Settings](assets/settings.png)
+
+- Scroll down and click on `Custom Shortcuts`
+![Keyboard ShortCuts](assets/keyboardShortCuts.png)
+
+- Click on add and fill in command and the keyboard shortcut that you want to use
+
+Name: `rofi-hacking-helper`
+
+Command: `/home/username/Desktop/base/code/xdotool/rofisearch_scripts_menu.sh` (change `username` to your username `~/` wont work)
+
+Shurtcut: - Press Ctrl+Shift+M and execute scripts from anywhere
+
+![Custom shortcut](assets/customShortcut.png)
 
 Some example scripts require other tools, like `zenity` or `pwsh`, but the idea is that you manage the scripts yourself.
 
 ## What it looks like
 
-![](boon.gif)
+![](assets/boon.gif)
+
+### fully automatic fix shell
+[Full size video](assets/shellFix.mp4)
+<video src='assets/shellFix.mp4' width=1200 />
+
+# TIP
+### Add ip,domain and creds and it will automaticly create custom codes that you can use in the rofi menu
+[dvdknaap/add-creds-rofi](https://github.com/dvdknaap/add-creds-rofi)
 
 ## Limitations
 - xdotool doesn't work in Wayland, so this works in Kali but not on Ubuntu.
@@ -28,3 +70,7 @@ Some example scripts require other tools, like `zenity` or `pwsh`, but the idea 
 
 ## Other productivity tips
 See [productivity tips](Productivity.md) for more general Kali productivity tips.
+
+# Forked and special thanks to @spipm
+[spipm/rofi-hacking-helper Repository](https://github.com/spipm/rofi-hacking-helper)
+
---
+++ install.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Function to check if a package is installed and install it if missing
+check_and_install_package() {
+    local package="$1"
+
+    if [[ -z "$package" ]]; then
+        echo "Usage: check_and_install_package <package_name>"
+        return 1
+    fi
+
+    echo "Checking if '$package' is installed..."
+    if ! command -v "$package" &>/dev/null; then
+        echo "'$package' is not installed. Installing..."
+        sudo apt update && sudo apt install -y "$package"
+    else
+        echo "'$package' is already installed."
+    fi
+}
+
+# Function to clone or update the repository
+clone_or_update_repo() {
+    local repo_url="https://github.com/dvdknaap/rofi-hacking-helper.git"
+    local target_dir="$HOME/Desktop/base"
+
+    if [ -d "$target_dir" ]; then
+        echo "Target directory $target_dir already exists. Pulling latest changes..."
+        git -C "$target_dir" pull
+    else
+        echo "Cloning repository to $target_dir..."
+        git clone "$repo_url" "$target_dir"
+    fi
+}
+
+# Function to set up a keyboard shortcut using XFCE
+setup_xfce_shortcut() {
+    local shortcut_command="$1"
+    local keybind="$2"
+
+    echo "Setting up keyboard shortcut..."
+    xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Shift>m" -t string -s "$shortcut_command" --create
+
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mShortcut successfully created! Use $keybind to launch the menu.\e[0m"
+    else
+        echo -e "\e[31mFailed to create shortcut. Please check your XFCE settings manually.\e[0m"
+    fi
+}
+
+# Function to check if a keybinding already exists using gsettings
+check_gsettings_keybinding() {
+    local search_command="$1"
+
+    echo "Checking if keybinding with command '$search_command' already exists..."
+    local keybindings
+    keybindings=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings | tr -d "[],'")
+
+    for key in $keybindings; do
+        local key_path="org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.$key"
+        local command
+        command=$(gsettings get "$key_path" command | tr -d "'")
+
+        if [[ "$command" == *"$search_command" ]]; then
+            echo -e "\e[32mKeybinding already exists:\e[0m $command"
+            return 0
+        fi
+    done
+
+    echo -e "\e[33mNo existing keybinding found for command '$search_command'.\e[0m"
+    return 1
+}
+
+# Main function to execute the script steps
+main() {
+    local shortcut_command="$HOME/Desktop/base/code/xdotool/rofisearch_scripts_menu.sh"
+    local keybind="Ctrl+Shift+M"
+
+    check_and_install_package rofi
+    check_and_install_package xdotool
+    check_and_install_package zenity
+    check_and_install_package powershell
+    
+    clone_or_update_repo
+
+    # Check for existing keybinding
+    check_gsettings_keybinding "$shortcut_command"
+    if [ $? -ne 0 ]; then
+        setup_xfce_shortcut "$shortcut_command" "$keybind"
+    else
+        echo "Keybinding setup skipped as it already exists."
+    fi
+
+    echo -e "\n\e[32mSetup is complete. You can now use the ROFI menu with $keybind in your terminal.\e[0m"
+}
+
+# Run the main function
+main
