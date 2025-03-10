c482a7c5d68576c8c06bb093e03fb1e7254f68e3
Danny van der Knaap <dannyvdknaap@gmail.com>
2025-03-10 22:56:34 +0100

    feat(bash): add update script

---
+++ update.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Set base directory
+BASEDIR=~/Desktop/base/code/xdotool
+
+# Function to check if a package is installed and install it if missing
+check_and_install_package() {
+    local package="$1"
+
+    if [[ -z "$package" ]]; then
+        echo "Usage: check_and_install_package <package_name>"
+        return 1
+    fi
+
+    echo "Checking if '$package' is installed..."
+    if ! command -v "$package" &>/dev/null; then
+        echo "'$package' is not installed. Installing..."
+        sudo apt update && sudo apt install -y "$package"
+    else
+        echo "'$package' is already installed."
+    fi
+}
+
+# pull latest git version
+pull_latest_git_version() {
+    cd "$BASEDIR"
+    git fetch origin
+    git pull origin main
+}
+
+# Main function to execute the script steps
+main() {
+    pull_latest_git_version
+
+    check_and_install_package rofi
+    check_and_install_package xdotool
+    check_and_install_package python3
+    check_and_install_package python3-tk
+    check_and_install_package powershell
+    check_and_install_package xclip
+
+    echo -e "\n\e[32mUpdate is complete.\e[0m"
+}
+
+# Run the main function
+main
