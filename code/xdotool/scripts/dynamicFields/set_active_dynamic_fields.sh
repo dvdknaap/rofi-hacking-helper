cb234e92385ded2416dbea4c4ea2e66c62f1547b
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-03-27 07:08:25 -0400

    fix(bash): update hacking documentation folder (#60)

--- code/xdotool/scripts/dynamicFields/set_active_dynamic_fields.sh
+++ code/xdotool/scripts/dynamicFields/set_active_dynamic_fields.sh
@@ -7,7 +7,7 @@ show a list of dynamic fields and select one to use as default values
 # Show a list of dynamic fields
 selected_dynamic_field=$(python3 "${HELPERS_DIR}"/generate_gui_dynamic_fields_form.py "${DYNAMIC_FIELDS_DIR}")
 
-cp "${selected_dynamic_field}" "${ACTIVE_DYNAMIC_FIELDS_FILE}"
+ln -sf "${selected_dynamic_field}" "${ACTIVE_DYNAMIC_FIELDS_FILE}"
 
 # notify user
 show_success_notify_message "selected dynamic fields are set as default"
