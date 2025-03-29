2970fdd16f37fc9d8ee53f72443369b2ebd1aefe
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-03-29 09:22:36 -0400

    feat(bash): add screenshot functionality (#72)

---
+++ code/xdotool/createScreenshot.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+json_file="${ACTIVE_DYNAMIC_FIELDS_FILE}"
+screenshot_path="${SCREENSHOTS_DIR}"
+screenshot_filename=""
+
+# Check if the JSON file exists
+if [[ -f "${json_file}" ]]; then
+    output_json=$(cat "${json_file}")
+
+    # Extract important fields (like ip, username, password, domain) from the JSON
+    ip=$(echo "${output_json}" | jq -r '.ip')
+    username=$(echo "${output_json}" | jq -r '.username')
+    password=$(echo "${output_json}" | jq -r '.password')
+    domain=$(echo "${output_json}" | jq -r '.domain')
+    current_date_time="$(date +"%Y_%m_%d_%H_%M_%S")"
+
+    # Generate the new file name based on md5sum of ip, username, password, and domain
+    screenshot_filename="${screenshot_path}/$(echo -n "${ip}_${username}_${password}_${domain}_${current_date_time}").png"
+else
+    # Generate GUI form items (label, type (optional: default text), name, default (optional))
+    SCREENSHOT_FILE_FIELD=$(form_item "screenshot filename" "screenshot_file" "$(date +"%Y_%m_%d_%H_%M_%S")")
+
+    # Generate GUI form
+    generate_form "${SCREENSHOT_FILE_FIELD}"
+
+    SCREENSHOT_FILE="${form_data["screenshot_file"]}"
+
+    screenshot_filename="${screenshot_path}/${SCREENSHOT_FILE}.png"
+
+    if [[ -f "${screenshot_filename}" ]]; then
+        show_error_notify_message "screenshot file already exists: ${screenshot_filename}"
+        exit 1
+    fi
+fi
+
+# wait for focus
+sleep 0.8
+
+import -window root "${screenshot_filename}"
+show_success_notify_message "screenshot created: ${screenshot_filename}"
--- update.sh
+++ update.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Set base directory
-BASEDIR=~/Desktop/base
-XDOTOOL_DIR="${BASEDIR}/code/xdotool"
+ROOT_DIR="${HOME}/Desktop/base"
+XDOTOOL_DIR="${ROOT_DIR}/code/xdotool"
 
-source "${XDOTOOL_DIR}/helpers/linux.sh"
+source "${XDOTOOL_DIR}/env.sh"
 
 # Function to check if a package is installed and install it if missing
-check_and_install_package() {
-    local package="$1"
-
-    if [[ -z "$package" ]]; then
-        show_info_notify_message "Usage: check_and_install_package <package_name>"
+check_and_install_packages() {
+    if [[ $# -eq 0 ]]; then
+        show_info_notify_message "Usage: check_and_install_packages <package1> <package2> ..."
         return 1
     fi
 
-    echo "Checking if '${package}' is installed..."
-    if ! command -v "${package}" &>/dev/null; then
-        echo "'${package}' is not installed. Installing..."
-        sudo apt update && sudo apt install -y "${package}"
-    fi
+    for package in "$@"; do
+        echo "Checking if '${package}' is installed..."
+        if ! command -v "${package}" &>/dev/null; then
+            echo "'${package}' is not installed. Installing..."
+            sudo apt update && sudo apt install -y "${package}"
+        fi
+    done
 }
 
 # Function to install pip3 packages
@@ -37,31 +37,119 @@ install_pip3_packages() {
 
 # pull latest git version
 pull_latest_git_version() {
-    cd "$BASEDIR"
+    cd "${ROOT_DIR}"
 
     git checkout main
     git fetch origin
     git pull origin main
 }
 
+# Function to set up a keyboard shortcut using XFCE
+setup_xfce_shortcut() {
+    local shortcut_command="$1"
+    local keybind="$2"
+    local letter="$3"
+
+    echo "Setting up keyboard shortcut..."
+    xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Shift>${letter}" -t string -s "${shortcut_command}" --create
+
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mShortcut successfully created! Use ${keybind} to launch the menu.\e[0m"
+    else
+        show_error_notify_message "Failed to create shortcut. Please check your XFCE settings manually."
+        echo -e "\e[31mFailed to create shortcut. Please check your XFCE settings manually.\e[0m"
+    fi
+}
+
+# Function to check if a keybinding already exists using gsettings
+setup_gnome_binding() {
+    local shortcut_name="$1"
+    local shortcut_command="$2"
+    local shortcut_keybind="$3"
+
+    local found_binding="0"
+    local keybindings=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings | tr -d "[],'")
+
+    echo "Checking if keybinding with command '${shortcut_name}' already exists..."
+
+    for key in $keybindings; do
+        if [[ "@as" == "${key}" ]]; then
+            continue
+        fi
+
+        local key_path="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${key}"
+        local name=$(gsettings get "${key_path}" name | tr -d "'")
+        
+        echo "key_path: ${key_path}"
+        echo "name: ${name}"
+
+        # /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/
+        if [[ "${name}" == *"${shortcut_name}" ]]; then
+            local command=$(gsettings get "${key_path}" command | tr -d "'")
+            local binding=$(gsettings get "${key_path}" binding | tr -d "'")
+
+            if [[ "${command}" != *"${search_command}" ]]; then
+                gsettings set "${key_path}" command "${shortcut_command}"
+                return 0
+            fi
+
+            if [[ "${binding}" != *"${shortcut_keybind}" ]]; then
+                gsettings set "${key_path}" binding "${shortcut_keybind}"
+                return 0
+            fi
+
+            found_binding="1"
+            break;
+        fi
+    done
+
+    if [[ "${found_binding}" == "0" ]]; then
+        local new_custom_key="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/${shortcut_name}/"
+        local key_path="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${new_custom_key}"
+        local keybindings=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings | tr -d "[],'")
+        local new_custom_keybindings="["
+
+        for key in $keybindings; do
+            if [[ "@as" == "${key}" ]]; then
+                continue
+            fi
+
+            new_custom_keybindings+="'${key}',"
+        done
+
+        new_custom_keybindings+="'${new_custom_key}'"
+        new_custom_keybindings+="]"
+
+        gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "${new_custom_keybindings}"
+        gsettings set "${key_path}" name "${shortcut_name}"
+        gsettings set "${key_path}" command "${shortcut_command}"
+        gsettings set "${key_path}" binding "${shortcut_keybind}"
+    fi
+
+    return 1
+}
+
 # Main function to execute the script steps
 main() {
+    local helper_name="rofi-hacking-helper"
+    local helper_shortcut_command="bash -i -c \"source ${ROOT_DIR}/code/xdotool/env.sh && source ${ROOT_DIR}/code/xdotool/rofisearch_scripts_menu.sh\""
+    local helper_keybind="M"
+
+    local screenshot_name="rofi-hacking-helper-screenshot"
+    local screenshot_shortcut_command="bash -i -c \"source ${ROOT_DIR}/code/xdotool/env.sh && source ${ROOT_DIR}/code/xdotool/createScreenshot.sh\""
+    local screenshot_keybind="N"
+
+    # Check for existing keybinding
+    setup_xfce_shortcut "${helper_shortcut_command}" "Ctrl+Shift+${helper_keybind}" "m"
+    setup_gnome_binding "${helper_name}" "${helper_shortcut_command}" "<Shift><Control>${helper_keybind}"
+
+    # Check for existing keybinding
+    setup_xfce_shortcut "${screenshot_shortcut_command}" "Ctrl+Shift+${screenshot_keybind}" "n"
+    setup_gnome_binding "${screenshot_name}" "${screenshot_shortcut_command}" "<Shift><Control>${screenshot_keybind}"
+
     pull_latest_git_version
 
-    check_and_install_package rofi
-    check_and_install_package xdotool
-    check_and_install_package python3
-    check_and_install_package python3-tk
-    check_and_install_package powershell
-    check_and_install_package xclip
-    check_and_install_package expect
-    check_and_install_package seclists
-    check_and_install_package jq
-    check_and_install_package onesixtyone
-    check_and_install_package braa
-    check_and_install_package wafw00f
-    check_and_install_package nikto
-    check_and_install_package finalrecon
+    check_and_install_packages rofi xdotool python3 python3-tk powershell xclip expect seclists jq onesixtyone braa wafw00f nikto finalrecon
 
     install_pip3_packages pyftpdlib
     install_pip3_packages sv-ttk
