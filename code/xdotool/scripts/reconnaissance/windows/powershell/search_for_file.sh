1a10338adb9f4a0dfbfa41d5dff6b8ac7bafa890
Danny van der Knaap <dannyvdknaap@gmail.com>
2025-04-04 08:45:03 +0200

    feat(bash): new scripts

--- code/xdotool/scripts/reconnaissance/windows/powershell/search_for_file.sh
+++ code/xdotool/scripts/reconnaissance/windows/powershell/search_for_file.sh
@@ -6,11 +6,13 @@ Powershell: search for a file or extension
 
 # Generate GUI form items (label, type (optional: default text), name, default (optional))
 FILE_FIELD=$(form_item  "file" "file" "*7z")
+PATH_FIELD=$(form_item  "path" "path" "c:\\")
 
 # Generate GUI form
-generate_form "${FILE_FIELD}"
+generate_form "${FILE_FIELD}" "${PATH_FIELD}"
 
 FILE=${form_data["file"]}
+PATH=${form_data["path"]}
 
-execute_command "\Get-ChildItem -Recurse -Include ${FILE} -ErrorAction SilentlyContinue -File"
+execute_command "Get-ChildItem -Recurse -Include "${FILE}" -Path "${PATH}" -ErrorAction SilentlyContinue -File"
 create_new_line
