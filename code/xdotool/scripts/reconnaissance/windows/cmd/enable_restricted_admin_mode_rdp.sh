48eb480e9d4a9301e4afb2367a76d4c08f3d1616
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-04-04 04:42:52 -0400

    feat(bash): new scripts (#133)

--- code/xdotool/scripts/reconnaissance/windows/cmd/enable_restricted_admin_mode_rdp.sh
+++ code/xdotool/scripts/reconnaissance/windows/cmd/enable_restricted_admin_mode_rdp.sh
@@ -4,5 +4,5 @@
 enable restricted admin mode
 '
 
-execute_command "reg add HKLM\System\CurrentControlSet\Control\Lsa /t REG_DWORD /v DisableRestrictedAdmin /d 0x0 /f"
+execute_command "reg add \"HKLM\\System\\CurrentControlSet\\Control\\Lsa\" /t REG_DWORD /v DisableRestrictedAdmin /d 0x0 /f"
 create_new_line
