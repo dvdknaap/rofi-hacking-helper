8b6b6ad68e26d02e014b0e68af7e003966114aff
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-04-04 03:47:33 -0400

    feat(bash): new scripts (#129)

--- code/xdotool/scripts/reconnaissance/windows/cmd/enable_rdp_with_password_login.sh
+++ code/xdotool/scripts/reconnaissance/windows/cmd/enable_rdp_with_password_login.sh
@@ -4,5 +4,5 @@
 Powershell: enable rdp with password
 '
 
-execute_command "reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /t REG_DWORD /v fDenyTSConnections /d 0.0 /f"
+execute_command "reg add "HKLM\\System\\CurrentControlSet\\Control\\Terminal Server" /t REG_DWORD /v fDenyTSConnections /d 0.0 /f"
 create_new_line
