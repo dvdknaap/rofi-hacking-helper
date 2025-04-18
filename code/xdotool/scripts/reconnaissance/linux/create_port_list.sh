57be3177705d7e3edeab2758abffa58bfc13c1af
Danny van der Knaap <dvdknaap@users.noreply.github.com>
2025-04-18 04:08:51 -0400

    feat(bash): new scripts (#197)

--- code/xdotool/scripts/reconnaissance/linux/create_port_list.sh
+++ code/xdotool/scripts/reconnaissance/linux/create_port_list.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 : '
-create a list of numbers. e.g. 2FA codes or ports and save to file
+create ports list and save to file
 '
 
 # Generate GUI form items (label, type (optional: default text), name, default (optional))
 SEQUENCE_START_FIELD=$(form_item  "sequence start" "number" "sequence_start" "1")
 SEQUENCE_END_FIELD=$(form_item  "sequence end" "number" "sequence_end" "65535")
+EQUAL_WIDTH_FIELD=$(form_item  "equal lengths" "select" "equal_lengths" "no" "no|yes")
 OUTPUT_FILE_FIELD=$(form_item  "output file" "output_file" "ports.txt")
 
 # Generate GUI form
-generate_form "${SEQUENCE_START_FIELD}" "${SEQUENCE_END_FIELD}" "${OUTPUT_FILE_FIELD}"
+generate_form "${SEQUENCE_START_FIELD}" "${SEQUENCE_END_FIELD}" "${EQUAL_WIDTH_FIELD}" "${OUTPUT_FILE_FIELD}"
 
 SEQUENCE_START=${form_data["sequence_start"]}
 SEQUENCE_END=${form_data["sequence_end"]}
+EQUAL_WIDTH=${form_data["equal_lengths"]}
 OUTPUT_FILE=${form_data["output_file"]}
 
-execute_command "seq ${SEQUENCE_START} ${SEQUENCE_END} > ${OUTPUT_FILE}"
+equal_length_switch=""
+if [[ "${EQUAL_WIDTH}" == "yes" ]]; then
+    equal_length_switch="-w "
+fi
+
+execute_command "seq ${equal_length_switch}${SEQUENCE_START} ${SEQUENCE_END} > ${OUTPUT_FILE}"
 create_new_line
